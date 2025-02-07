78080443949b6de9dd8011c8edfb6a2a8bf5d740
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-02-07 03:41:53 +0200

    новый класс для ассерта

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/DeadlinesTasks/ChangeRespAssert.cs
@@ -0,0 +1,41 @@
+﻿using atFrameWork2.SeleniumFramework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects.DeadlinesTasks
+{
+    internal class ChangeRespAssert
+    {
+        WebItem responsibleElement => new WebItem("//div[@class='ui-selector-item-box ui-selector-item-box-selected']", "Для ассерта");
+        public void VerifyResponsibleChange()
+        {
+            string actualResponsible = GetResponsibleFromUI();
+
+            if (!string.IsNullOrEmpty(actualResponsible))
+            {
+                Console.WriteLine($"Изменения сохранены: новый исполнитель - {actualResponsible}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка, изменения не сохранены");
+            }
+        }
+
+        private string GetResponsibleFromUI()
+        {
+            try
+            {
+
+                return responsibleElement.ToString();
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
