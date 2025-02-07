16ecaa2e44554fb2d734ccc9cf7e39afc357f73f
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-02-07 03:45:03 +0200

    изменение ответственного на странице "сроки"

--- BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/DeadlinesTasks/DeadlineTasksPage.cs
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/DeadlinesTasks/DeadlineTasksPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using atFrameWork2.SeleniumFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,13 @@ namespace ATframework3demo.PageObjects.DeadlinesTasks
 {
     public class DeadlineTasksPage
     {
-        public DeadlinesTasksCreateTask CreateNewTask()
-        {
-            throw new NotImplementedException();
-            return new DeadlinesTasksCreateTask();
-        }
 
+        WebItem ChangeRespBtn => new WebItem("//div[@class='tasks-kanban-item-responsible']/div[@class='tasks-kanban-item-author-avatar']", "Открытие списка ответственных");
+        WebItem ResponsibleList => new WebItem("//div[@class='ui-selector-item-box']", "Список отвественных");
         public DeadlineTasksListChangeResponsible ChangeResponsible()
         {
-            throw new NotImplementedException();
+            ChangeRespBtn.Click();
+            ResponsibleList.Click();
             return new DeadlineTasksListChangeResponsible();
         }
 
