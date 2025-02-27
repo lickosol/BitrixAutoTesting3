c3bb095e42e70427ec7e844a082fe9d7aaa7d0a0
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-02-27 12:35:47 +0200

    пейдж открытия ленты в iframe проекта

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/CreationProjectPgOb/ProjectOpenLenta.cs
@@ -0,0 +1,21 @@
+﻿using atFrameWork2.SeleniumFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ATframework3demo.PageObjects.CreationProjectPgOb
+{
+    public class ProjectOpenLenta : BaseProjectFramePage
+    {
+        public void OpenLenta()
+        {
+ 
+            WebItem btnLentaInProject = new WebItem("//span[contains(@class, 'main-buttons-item-text-box') and contains(text(), 'Лента')]", 
+                "нажимаем на кнопку лента в верхнем меню проекта");
+            btnLentaInProject.Click();
+        }
+    }
+}
