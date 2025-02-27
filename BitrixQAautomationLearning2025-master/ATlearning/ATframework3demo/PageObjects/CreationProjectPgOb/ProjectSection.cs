bc35cbb12cd9e03c6da479bcda03e2aad6ac1912
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-02-27 12:36:20 +0200

    пейдж открытия только что созданного проекта

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/CreationProjectPgOb/ProjectSection.cs
@@ -0,0 +1,28 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.SeleniumFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects.CreationProjectPgOb
+{
+    public class ProjectSection : BaseProjectFramePage
+    {
+        private readonly int _projectId;
+
+        public ProjectSection(int projectId)
+        {
+            _projectId = projectId;
+        }
+
+        public void OpenProject()
+        {
+            WebItem btnOpenProject = new WebItem($"//a[contains(@href,'/workgroups/group/{_projectId}/tasks/')]", "Открытие проекта");
+            btnOpenProject.Click();
+
+            SwitchToProjectFrame();
+        }
+    }
+}
