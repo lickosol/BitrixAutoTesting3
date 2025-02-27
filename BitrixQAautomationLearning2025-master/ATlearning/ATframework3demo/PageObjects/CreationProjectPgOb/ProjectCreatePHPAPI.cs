04c8aee3a047464abea708bc37d4e47d01e8ecc9
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-02-27 12:35:24 +0200

    php api создания проекта

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/CreationProjectPgOb/ProjectCreatePHPAPI.cs
@@ -0,0 +1,43 @@
+﻿using AquaTestFramework.CommonFramework.BitrixCPinteraction;
+using atFrameWork2.BaseFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects.CreationProjectPgOb
+{
+    internal class ProjectCreatePHPAPI
+    {
+        public static int ProjectCreate()
+        {
+            var phpExecutor = new PHPcommandLineExecutor(
+                TestCase.RunningTestCase.TestPortal.PortalUri,
+                TestCase.RunningTestCase.TestPortal.PortalAdmin.LoginAkaEmail,
+                TestCase.RunningTestCase.TestPortal.PortalAdmin.Password);
+
+            string projectCreationPhpCode = $"require($_SERVER[\"DOCUMENT_ROOT\"].\"/bitrix/modules/main/include/prolog_before.php\");" +
+                $"\r\n\r\nglobal $USER;\r\n$userID = $USER->GetID();" +
+                $"\r\n\r\nif (CModule::IncludeModule(\"socialnetwork\")) " +
+                $"{{\r\n    $arFields = [\r\n" +
+                $"\"NAME\" => \"Личный проект \" . date(\"Y-m-d H:i:s\"),\r\n" +
+                $"\"DESCRIPTION\" => \"Описание личного проекта\",\r\n" +
+                $"\"VISIBLE\" => \"Y\",\r\n" +
+                $"\"OPENED\" => \"Y\",\r\n" +
+                $" \"PROJECT\" => \"Y\",\r\n" +
+                $" \"OWNER_ID\" => $userID,\r\n" +
+                $" \"SUBJECT_ID\" => 1,\r\n" +
+                $"\"INITIATE_PERMS\" => \"A\",\r\n" +
+                $"\"CLOSED\" => \"N\",\r\n" +
+                $"\"SPAM_PERMS\" => \"N\",\r\n" +
+                $" \"SITE_ID\" => \"s1\", \r\n    ];\r\n\r\n   " +
+                $" $groupID = CSocNetGroup::CreateGroup($userID, $arFields);" +
+                $"\r\necho \"PROJECT_ID:\" . $groupID;\r\n\r\n\r\n}} \r\n\r\n";
+
+            string result = phpExecutor.Execute(projectCreationPhpCode);
+            int projectId = int.Parse(result.Split(new[] { "PROJECT_ID:" }, StringSplitOptions.None)[1].Trim());
+            return projectId;
+        }
+    }
+}
