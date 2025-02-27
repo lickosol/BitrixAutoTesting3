115f3bf1584dec9d7edfd0731162d4ec5ef02624
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-02-27 12:35:07 +0200

    assert появления поста в ленте

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/CreationProjectPgOb/NewPostInProjectAssert.cs
@@ -0,0 +1,27 @@
+﻿using atFrameWork2.SeleniumFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ATframework3demo.PageObjects.CreationProjectPgOb
+{
+    public class NewPostInProjectAssert
+    {
+        private readonly string _postTitle;
+
+        public NewPostInProjectAssert(string postTitle = "ответьте да")
+        {
+            _postTitle = postTitle;
+        }
+
+        public void VerifyMessageCreate()
+        {
+            WebItem createdPost = new WebItem($"//div[contains(text(), '{_postTitle}')]", "проверяем наличие созданного поста");
+            createdPost.WaitElementDisplayed();
+            Console.WriteLine($"успешно: пост с заголовком '{_postTitle}' создан и отображается.");
+        }
+    }
+}
