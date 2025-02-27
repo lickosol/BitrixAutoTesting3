e4d39c39a7d6ed44455f5a608a053384944ee291
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-02-27 12:34:36 +0200

    пейдж переключения в iframe проекта

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/CreationProjectPgOb/BaseProjectFramePage.cs
@@ -0,0 +1,19 @@
+﻿using atFrameWork2.SeleniumFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects.CreationProjectPgOb
+{
+    public class BaseProjectFramePage
+    {
+        protected void SwitchToProjectFrame()
+        {
+            WebItem projectFrame = new WebItem("//iframe[@class='side-panel-iframe']", "переключаемся на фрейм проекта");
+            projectFrame.WaitElementDisplayed();
+            projectFrame.SwitchToFrame();
+        }
+    }
+}
