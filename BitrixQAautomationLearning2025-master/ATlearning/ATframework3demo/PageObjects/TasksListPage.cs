84c12e1c84a7d4530ac5fa251f602f71ad8a925e
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-02-27 12:32:03 +0200

    добавление открытия раздела Проекты в верхнем меню

--- BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/TasksListPage.cs
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/TasksListPage.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using ATframework3demo.PageObjects.DeadlinesTasks;
 using atFrameWork2.SeleniumFramework;
+using ATframework3demo.PageObjects.CreationProjectPgOb;
 
 namespace atFrameWork2.PageObjects
 {
@@ -19,5 +20,14 @@ namespace atFrameWork2.PageObjects
             btnDeadlines.Click();
             return new DeadlineTasksPage();
         }
+        public ProjectSection OpenProjectsSection(int projectId)
+        {
+            var btnProjects = new WebItem("//div[contains(@id, 'tasks_panel_menu')]//span[contains(@class, 'main-buttons-item-text-box') and contains(text(), 'Проекты')]",
+                "Открытие раздела Проекты из верхнего меню");
+            btnProjects.Click();
+            return new ProjectSection(projectId);
+        }
     }
+
+
 }
