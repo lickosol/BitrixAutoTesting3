ab99d786e012b0956ca490f01e5dae6babd5e9fd
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-14 14:31:59 +0200

    пейдж бизнес процессы в автоматизации

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/BusinessProcess/ProcessesPage.cs
@@ -0,0 +1,29 @@
+﻿using atFrameWork2.SeleniumFramework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects.BusinessProcess
+{
+    public class ProcessesPage
+    {
+        public ProcessesPage ProcessesList()
+        {
+            var outgoingDocProcess = new WebItem("//a[@href='/bizproc/processes/12/view/0/?list_section_id='][contains(text(), 'Исходящие документы')]",
+    "Выбираем процесс Исходящие документы");
+            outgoingDocProcess.Click();
+            return new ProcessesPage();
+        }
+
+        public OutDocPage OutDocList(int processId)
+        {
+            // выбор только что созданного процесса в пейдже со списком процессов Исходящих документов
+            var createdProcess = new WebItem($"//tr[@data-id='{processId}']//a[contains(@href, '/bizproc/processes/12/element/0/')]", "Выбираем созданный бизнес-процесс");
+            createdProcess.Click();
+            return new OutDocPage();
+        }
+    }
+}
