aa7df97fe45decc0bb355ea4d1446855e17c4077
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-14 14:34:42 +0200

    пейдж "Автоматизация" в левом меню

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/AutomatisationListPage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using ATframework3demo.PageObjects.DeadlinesTasks;
+using atFrameWork2.SeleniumFramework;
+using ATframework3demo.PageObjects.CreationProjectPgOb;
+using ATframework3demo.PageObjects.BusinessProcess;
+using System.Diagnostics;
+
+namespace atFrameWork2.PageObjects
+{
+    // страница раздела автоматизация
+    public class AutomatisationListPage
+    {
+        public AutomatisationListPage(IWebDriver driver = default)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; }
+
+        // запуск процесса "Исходящие документы"
+        public ProcessesPage BPOutDocList()
+        {
+            var btnBPlistopen = new WebItem("//span[@class='main-buttons-item-text-box'][contains(text(), 'Бизнес-процессы')]", "Открываем список процессов в верхнем меню"); 
+            btnBPlistopen.Click();
+
+            var popupBPinLenta = new WebItem("//a[@href='/bizproc/processes/']", "Выбираем Процессы в ленте новостей");
+            popupBPinLenta.Click();
+            return new ProcessesPage();
+
+
+        }
+
+
+
+        //открытие первого в очереди (нашего нового) процесса
+        public BusinessProcessPage OpenBP()
+        {
+            var btnBPopen = new WebItem("//a[@class='bp-user-processes__title-link ui-typography-text-lg']", "Открываем первый в списке бизнесс процесс, который мы создали"); 
+            btnBPopen.Click();
+
+            return new BusinessProcessPage();
+
+        }
+
+    }
+
+}
