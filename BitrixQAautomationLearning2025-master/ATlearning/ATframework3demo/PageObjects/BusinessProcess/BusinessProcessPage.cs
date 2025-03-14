e916c8a8ddb896f6e82aba0b31029e066d044249
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-14 14:33:06 +0200

    фрейм только что созданного нами бизнес процесса и его фиксация

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/BusinessProcess/BusinessProcessPage.cs
@@ -0,0 +1,36 @@
+﻿using atFrameWork2.SeleniumFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects.BusinessProcess
+{
+    // Раздел бизнес-процессов
+    public class BusinessProcessPage
+    {
+        public void FixationDocPage()
+        {
+            // Переключаемся в фрейм перед вводом номера документа
+            SwitchToBPFrame();
+
+            // Ввод номера документа
+            WebItem inputDocNum = new WebItem("//input[@class='bizproc-type-control bizproc-type-control-string bizproc-type-control-required']", "ввод номера документа во фрейме БП");
+            WebItem saveBPbtn = new WebItem("//button[@class='ui-btn ui-btn-md ui-btn-success ui-btn-round']", "сохранить фиксацию БП");
+
+            inputDocNum.Click();
+            inputDocNum.SendKeys("1");
+            saveBPbtn.Click();
+        }
+
+        // Переключение во фрейм запущенного процесса
+        private void SwitchToBPFrame()
+        {
+            WebItem bpFrame = new WebItem("//iframe[contains(@id, 'iframe')]", "Переключаемся на фрейм бизнес процесса");
+            bpFrame.WaitElementDisplayed();
+            bpFrame.SwitchToFrame();
+        }
+    }
+}
+
