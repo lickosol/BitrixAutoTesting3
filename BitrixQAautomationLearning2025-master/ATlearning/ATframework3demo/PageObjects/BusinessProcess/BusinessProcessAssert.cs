88354e30dffb5035fcd3f50898f88f1c130cb2a1
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-16 01:32:46 +0200

    изменение ассерта на bool для возможности проверки для удаления новости

--- BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/BusinessProcess/BusinessProcessAssert.cs
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/BusinessProcess/BusinessProcessAssert.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace ATframework3demo.Assertions
 {
+    //вывод в консоль проверки корректного завершения бизнес процесса
     public class BusinessProcessAssert
     {
-        public static void VerifyProcessCompleted()
+        public static bool VerifyProcessCompleted()
         {
-
             WebItem lastFeedTime = new WebItem("//div[@class='feed-time']", "время последней новости");
             string feedTimeText = lastFeedTime.GetAttribute("innerText").Trim();
 
@@ -28,28 +28,34 @@ namespace ATframework3demo.Assertions
 
                 try
                 {
-                    // Преобразуем строку во время
+                    //преобразование строки во время
                     DateTime newsTime = DateTime.ParseExact(newsTimeStr, "HH:mm", null);
                     Console.WriteLine($"преобразованное время новости: {newsTime:HH:mm}");
 
-                    // Проверяем разницу во времени (не более 1 минуты)
+                    //проверка разницы во времени (не более 2 минут)
                     double difference = Math.Abs((now - newsTime).TotalMinutes);
                     Console.WriteLine($"разница во времени: {difference} минут");
 
                     if (difference <= 2)
                     {
                         Console.WriteLine("бизнес-процесс успешно завершился");
+                        return true;
                     }
                     else
                     {
                         Console.WriteLine("ошибка в завершении процесса");
+                        return false;
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine($"ошибка при разборе времени: {ex.Message}");
+                    return false;
                 }
             }
+
+            return false;
         }
+
     }
 }
