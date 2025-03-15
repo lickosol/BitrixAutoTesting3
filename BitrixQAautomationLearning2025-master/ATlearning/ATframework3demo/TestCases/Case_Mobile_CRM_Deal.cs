323b299c67dc81eaea6bfed84928244a0a64f4d3
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-15 03:59:59 +0200

    тест Создание сделки в CRM мобильное приложение

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/TestCases/Case_Mobile_CRM_Deal.cs
@@ -0,0 +1,36 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.TestEntities;
+using ATframework3demo.PageObjects.Mobile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.TestCases
+{
+    public class Case_Mobile_CRM_Deal : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(
+                new TestCase("Создание сделки CRM", mobileHomePage => CreateTask(mobileHomePage)));
+            return caseCollection;
+        }
+
+        void CreateTask(MobileHomePage homePage)
+        {
+            var createCRMdeal = homePage.TabsPanel;
+
+            Waiters.StaticWait_s(30);
+
+            createCRMdeal.ClosePush()
+                .MorePanel()
+                .MobileOpenCRM()
+                .MobileOpenDeals()
+                .MobileCreateDeal();
+
+        }
+    }
+}
