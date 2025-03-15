a8826678c4a2109d79db5c7a6c1c475f52b7c620
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-15 03:59:05 +0200

    раздел Еще

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/Mobile_CRM_Deal/MobileMorePanel.cs
@@ -0,0 +1,19 @@
+﻿using atFrameWork2.SeleniumFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects.Mobile_CRM_Deal
+{
+    public class MobileMorePanel
+    {
+        public MobileCrmPage MobileOpenCRM()
+        {
+            var btnCRM = new MobileItem("//android.widget.TextView[@resource-id='com.bitrix24.android:id/title' and @text='CRM']", "раздел CRM в разделе Еще");
+            btnCRM.Click();
+            return new MobileCrmPage(); 
+        }
+    }
+}
