b351f02e802f2446fd564578f17ef4b71ceb385b
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-16 02:18:05 +0200

    изменение кодстаила

--- BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/BusinessProcess/ProcessesPage.cs
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/BusinessProcess/ProcessesPage.cs
@@ -12,8 +12,8 @@ namespace ATframework3demo.PageObjects.BusinessProcess
     {
         public ProcessesPage ProcessesList()
         {
-            var outgoingDocProcess = new WebItem("//a[@href='/bizproc/processes/12/view/0/?list_section_id='][contains(text(), 'Исходящие документы')]",
-    "Выбираем процесс Исходящие документы");
+            var outgoingDocProcess = new WebItem("//a[@href='/bizproc/processes/12/view/0/?list_section_id='][contains(text(), 'исходящие документы')]",
+                "Выбираем процесс Исходящие документы");
             outgoingDocProcess.Click();
             return new ProcessesPage();
         }
@@ -21,7 +21,7 @@ namespace ATframework3demo.PageObjects.BusinessProcess
         public OutDocPage OutDocList(int processId)
         {
             // выбор только что созданного процесса в пейдже со списком процессов Исходящих документов
-            var createdProcess = new WebItem($"//tr[@data-id='{processId}']//a[contains(@href, '/bizproc/processes/12/element/0/')]", "Выбираем созданный бизнес-процесс");
+            var createdProcess = new WebItem($"//tr[@data-id='{processId}']//a[contains(@href, '/bizproc/processes/12/element/0/')]", "выбираем созданный бизнес-процесс");
             createdProcess.Click();
             return new OutDocPage();
         }
