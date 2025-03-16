16dead5681c0dee4655d5e2a8ebd95bb2c834680
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-16 02:17:34 +0200

    редактирование кодстаила

--- BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/BusinessProcess/OutDocPage.cs
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/BusinessProcess/OutDocPage.cs
@@ -10,25 +10,25 @@ namespace ATframework3demo.PageObjects.BusinessProcess
 {
     public class OutDocPage
     {
-        // пейдж внутрянки процесса
+        //пейдж внутрянки процесса
         public OutDocPage StartedProcess()
         {
-            // обязательное поле Дата
+            //обязательное поле Дата
             var btnDate = new WebItem($"//input[@class='ui-ctl-element' and @name='PROPERTY_53[n0][VALUE]']", "Ввод даты");
             btnDate.Click();
             btnDate.SendKeys("08.04.2025");
 
-            var bpChapter = new WebItem($"//td[@id='tab_tab_bp'][contains(text(), 'Бизнес-процессы')]", "Открываем вкладку Бизнес-процессы");
+            var bpChapter = new WebItem($"//td[@id='tab_tab_bp'][contains(text(), 'Бизнес-процессы')]", "открываем вкладку Бизнес-процессы");
             bpChapter.Click();
 
-            // запуск процесса на вкладке бизнес процессы
-            var btnStartedBP = new WebItem($"//span[@class='ui-btn ui-btn-dropdown ui-btn-primary']", "Нажимаем Запустить процесс");
+            //запуск процесса на вкладке бизнес процессы
+            var btnStartedBP = new WebItem($"//span[@class='ui-btn ui-btn-dropdown ui-btn-primary']", "нажимаем Запустить процесс");
             btnStartedBP.Click();
 
-            var popupBtnOutDoc = new WebItem($"//span[@class='menu-popup-item-text'][contains(text(), 'Исходящие документы')]", "В контекстном меню выбираем Исходящие документы");
+            var popupBtnOutDoc = new WebItem($"//span[@class='menu-popup-item-text'][contains(text(), 'Исходящие документы')]", "в контекстном меню выбираем Исходящие документы");
             popupBtnOutDoc.Click();
 
-            var btnApproveBP = new WebItem($"//input[@title='Сохранить и остаться в форме']", "Применяем изменения");
+            var btnApproveBP = new WebItem($"//input[@title='Сохранить и остаться в форме']", "применяем изменения");
             btnApproveBP.Click();
 
             return new OutDocPage();
