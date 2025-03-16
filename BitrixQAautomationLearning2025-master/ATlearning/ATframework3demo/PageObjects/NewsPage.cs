79a658c7b6a0c3246fdf0f1494e66025707d1df3
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-16 02:20:35 +0200

    добавление очистки после теста

--- BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/NewsPage.cs
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/NewsPage.cs
@@ -1,4 +1,6 @@
 ﻿using atFrameWork2.SeleniumFramework;
+using ATframework3demo.Assertions;
+using ATframework3demo.SeleniumFramework.DriverActions;
 using OpenQA.Selenium;
 
 namespace ATframework3demo.PageObjects
@@ -18,6 +20,24 @@ namespace ATframework3demo.PageObjects
             var btnPostCreate = new WebItem("//div[@id='microoPostFormLHE_blogPostForm_inner']", "Область в новостях 'Написать сообщение'");
             btnPostCreate.Click(Driver);
             return new NewsPostForm(Driver);
+        }
+
+        //удаление новости после проверки завершения бизнес процесса "Исходящие документы"
+        public NewsPage DeleteNews() 
+        {
+            if (!BusinessProcessAssert.VerifyProcessCompleted())
+            {
+                Console.WriteLine("новость не будет удалена, так как бизнес-процесс не завершен успешно");
+                return this;
+            }
+
+            Console.WriteLine("очистка новостей от тестовой проверки");
+            var btnContextMenuNews = new WebItem("//div[@class='feed-post-right-top-menu']", "контекстное меню в новости через три точки");
+            btnContextMenuNews.Click();
+            var btnDeleteNews = new WebItem("//span[@ class='menu-popup-item-text'][contains(text(), 'Удалить из ленты новостей')]", "удалить новость");
+            btnDeleteNews.Click();
+            WebDriverActions.BrowserAlert(true);
+            return new NewsPage();
         }
     }
 }
