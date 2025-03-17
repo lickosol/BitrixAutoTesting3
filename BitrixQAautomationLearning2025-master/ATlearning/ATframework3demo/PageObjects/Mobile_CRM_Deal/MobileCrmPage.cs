5064c3444178e538358a02ca9881e6415e629f01
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-03-17 05:09:51 +0200

    CRM пейдж: добавлен ассерт + очистка после теста

--- BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/Mobile_CRM_Deal/MobileCrmPage.cs
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/Mobile_CRM_Deal/MobileCrmPage.cs
@@ -9,8 +9,8 @@ namespace ATframework3demo.PageObjects.Mobile_CRM_Deal
 {
     public class MobileCrmPage
     {
-
-        public MobileCRMdealPage MobileOpenDeals()
+        //переход в редактор создания сделки
+        public MobileCRMdealPage MobileOpenCreationDeals()
         {
             var btnAddinCRM = new MobileItem("//android.widget.ImageButton[@resource-id='com.bitrix24.android:id/component_fab']", "кнопка + в CRM");
             btnAddinCRM.Click();
@@ -19,6 +19,41 @@ namespace ATframework3demo.PageObjects.Mobile_CRM_Deal
             btnDeal.Click();
 
             return new MobileCRMdealPage(); 
+        }
+
+        //проверка создания сделки
+        public MobileCrmPage MobileAssertDeal()
+        {
+            var createdDeal = new MobileItem("//android.widget.TextView[@text='тест сделка']", "проверка наличия созданной сделки");
+
+            if (createdDeal.WaitElementDisplayed(5))
+            {
+                Console.WriteLine("сделка 'тест сделка' успешно создана и отображается в списке CRM");
+            }
+            else
+            {
+                Console.WriteLine("ошибка: сделка не создана");
+                throw new Exception("сделка не найдена");
+            }
+
+            return new MobileCrmPage();
+        }
+
+
+        //удаление сделки
+        public MobileCrmPage MobileDealDelete()
+        {
+            var contextMenuInDeal = new MobileItem("//android.widget.ImageButton[@content-desc='KANBAN_STAGE_CONTEXT_MENU_BTN']", "три точки контекстное меню в сделке");
+            contextMenuInDeal.Click();
+
+            var btnDeleteDeal = new MobileItem("(//android.widget.TextView[@content-desc='KANBAN_STAGE_CONTEXT_MENU_delete_title'])",
+                "выбираем удалить");
+            btnDeleteDeal.Click();
+
+            var acceptDeleteDeal = new MobileItem("//android.widget.Button[@resource-id='android:id/button2']", "подтверждаем удаление");
+            btnDeleteDeal.Click();
+
+            return new MobileCrmPage();
         }
     }
 }
