97eca5df87bbe1bc0d78020a45f247ee53313618
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-05-02 00:22:25 +0200

    функция перехода на фрейм создания события в модуле Досуг

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/Hobbies/CreateEventFrame.cs
@@ -0,0 +1,22 @@
+﻿using atFrameWork2.SeleniumFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects.Hobbies
+{
+    public class CreateEventFrame
+    {
+        //переход на фрейм создания события
+        protected CreateEventPage SwitchToCreationEventFrame()
+        {
+            var createEventFrame = new WebItem("//iframe[contains(@id, 'iframe')]", "переключаемся на фрейм создания события");
+            createEventFrame.WaitElementDisplayed();
+            createEventFrame.SwitchToFrame();
+
+            return new CreateEventPage();
+        }
+    }
+}
