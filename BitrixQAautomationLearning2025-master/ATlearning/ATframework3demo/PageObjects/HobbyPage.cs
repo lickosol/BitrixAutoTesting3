b4312172fbe85cc8265332ace6ad8d66479c0663
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-05-02 00:21:04 +0200

    пейдж модуля Досуг

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/HobbyPage.cs
@@ -0,0 +1,54 @@
+﻿using atFrameWork2.SeleniumFramework;
+using OpenQA.Selenium;
+using ATframework3demo.PageObjects.Hobbies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects
+{
+    //пэйдж страницы досуг
+    public class HobbyPage
+    {
+        public HobbyPage(IWebDriver driver = default)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; }
+
+        //создание нового события
+        public HobbyPage OpenHobbyPage()
+        {
+            //пейдж модуля досуг
+            var btnCreateEvent = new WebItem("//span[@class='ui-btn-text' and text()='Создать']", "тык на кнопку создать");
+            btnCreateEvent.Click();
+
+            return new HobbyPage();
+        }
+
+        //пейдж создания события
+        public CreateEventPage OpenEventFrame()
+        {
+            var createEventFrame = new WebItem("//iframe[contains(@id, 'iframe')]", "переключаемся на фрейм создания события");
+            createEventFrame.WaitElementDisplayed();
+            createEventFrame.SwitchToFrame();
+
+            return new CreateEventPage();
+        }
+
+        //завершение события
+        public HobbyPage EndEvent()
+        {
+            var menuEvent = new WebItem("//a[@class='main-grid-row-action-button' and contains(@data-actions, 'Завершить')]", "кнопка меню три полоски");
+            menuEvent.Click();
+
+            var endEvent = new WebItem("//span[@class='menu-popup-item-text' and text()='Завершить']", "тык завершить");
+            endEvent.Click();
+
+            return new HobbyPage();
+        }
+    }
+}
