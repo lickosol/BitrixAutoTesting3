17c11018b8cf23aad802e306a754307af5b80216
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-05-02 00:23:59 +0200

    проверка создания новости в Ленте о появлении нового события

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/Hobbies/HobbyEventAssert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using atFrameWork2.SeleniumFramework;
+
+
+namespace ATframework3demo.Assertions.Hobbies
+{
+    public class HobbyEventAssert
+    {
+        public static bool VerifyAchiveNewsCreated()
+        {
+            WebItem lastFeedText = new WebItem("//div[contains(@class, 'feed-post-text-block')]", "проверка текста последней новости");
+            string postText = lastFeedText.GetAttribute("innerText").Trim();
+
+            Console.WriteLine($"текст последней новости: '{postText}'");
+
+            if (postText.Contains("Досуг::Новое событие:проверка ачивки"))
+            {
+                Console.WriteLine("новость о тестовом событии найдена");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("ошибка получения данных новости о тестовом событии");
+                return false;
+            }
+        }
+    }
+}
+
+
