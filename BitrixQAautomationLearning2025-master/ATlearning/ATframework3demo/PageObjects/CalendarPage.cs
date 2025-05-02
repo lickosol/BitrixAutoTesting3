81e4e0b117d00bfe9b9ced6f69fb5047a8306511
Elena Zaynulina <elena.zaynulina03@gmail.com>
2025-05-02 16:26:01 +0200

    добавление пейджа календаря + метод удаления тестового события

---
+++ BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/PageObjects/CalendarPage.cs
@@ -0,0 +1,33 @@
+﻿using atFrameWork2.SeleniumFramework;
+using OpenQA.Selenium;
+using ATframework3demo.PageObjects.Hobbies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATframework3demo.PageObjects
+{
+    public class CalendarPage
+    {
+        public CalendarPage(IWebDriver driver = default)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; }
+
+        //удаление события из календаря
+        public CalendarPage DeleteEventFromCalendar()
+        {
+            var choiceTestEvent = new WebItem("//span[contains(text(), 'Досуг') and contains(text(), 'тестовое событие')]", "выбор тестового события");
+            var btnDeleteEvent = new WebItem("//button[.//span[@class='ui-btn-text' and text()='Удалить']]", "тык на кнопку удалить");
+
+            choiceTestEvent.Click();
+            btnDeleteEvent.Click();
+
+            return new CalendarPage(Driver);
+        }
+    }
+}
